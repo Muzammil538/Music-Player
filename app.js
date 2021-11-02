faabc5f9a0a75aa51f60a976f5c287487962bedb
Muzammil538 <70688333+Muzammil538@users.noreply.github.com>
2021-11-02 22:30:39 +0530

    added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const playBtn = document.querySelector("#play");
 const progressBoll = document.querySelector(".progress-boll");
 const pauseBtn = document.querySelector("#pause");
-const music = new Audio("music1.mp3");
+var music = new Audio("music1.mp3");
 const Img = document.querySelector("img");
+const List = document.querySelector("#list");
+const lost_sky = document.querySelector("#lost_sky");
+
+List.addEventListener("click", ()=> {
+    var list_item = document.querySelector("#list-icon");
+    list_item.classList.toggle("active");
+    PlayNew();
+})
+
 playBtn.addEventListener("click", (e)=>{
     e.preventDefault();
     progressBoll.classList.add("move");
@@ -21,3 +30,12 @@ pauseBtn.addEventListener("click",(e)=>{
     pauseBtn.classList.add("hide");
     playBtn.classList.remove("hide");
 });
+
+function PlayNew(){
+    lost_sky.addEventListener("click", ()=>{
+        music = new Audio("LostSky.mp3");
+        music.play();
+        pauseBtn.classList.remove("hide");
+    playBtn.classList.add("hide");
+    })
+}
