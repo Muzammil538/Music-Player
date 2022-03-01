5a9a29460b1e0ccb87589561b1bc55c260e13425
MAM <70688333+Muzammil538@users.noreply.github.com>
2022-03-01 19:37:52 +0530

    added

--- app.js
+++ app.js
@@ -1,50 +1,18 @@
 const playBtn = document.querySelector("#play");
-const progressBoll = document.querySelector(".progress-boll");
 const pauseBtn = document.querySelector("#pause");
-var music = new Audio("music1.mp3");
-const Img = document.querySelector("img");
-const List = document.querySelector("#list");
-const lost_sky = document.querySelector("#lost_sky");
-const mmh = document.querySelector("#mmh");
-
-List.addEventListener("click", ()=> {
-    var list_item = document.querySelector("#list-icon");
-    list_item.classList.toggle("active");
-    PlayNew();
-})
+var music = new Audio();
 
 playBtn.addEventListener("click", (e)=>{
     e.preventDefault();
-    progressBoll.classList.add("move");
-    Img.classList.add("rotate");
-    music.play();
+    music.play(`${playBtn.parentElement}`);
     pauseBtn.classList.remove("hide");
     playBtn.classList.add("hide");
 });
 
 pauseBtn.addEventListener("click",(e)=>{
     e.preventDefault();
-    progressBoll.classList.remove("move");
     //var music = new Audio("music1.mp3");
-    Img.classList.remove("rotate");
     music.pause();
     pauseBtn.classList.add("hide");
     playBtn.classList.remove("hide");
 });
-
-function PlayNew(){
-    lost_sky.addEventListener("click", ()=>{
-        music = new Audio("LostSky.mp3");
-        music.play();
-        pauseBtn.classList.remove("hide");
-    playBtn.classList.add("hide");
-    })
-}
-function PlayNew(){
-    mmh.addEventListener("click", ()=>{
-        music = new Audio("mmh.mp3");
-        music.play();
-        pauseBtn.classList.remove("hide");
-    playBtn.classList.add("hide");
-    })
-}
