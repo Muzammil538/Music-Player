4a1f152632184b820ec206da44fa76fa7db980bf
MAM <70688333+Muzammil538@users.noreply.github.com>
2022-03-05 22:39:34 +0530

    aa

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-const playBtn = document.querySelector("#play");
-const pauseBtn = document.querySelector("#pause");
-var music = new Audio();
-
-playBtn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    music.play(`${playBtn.parentElement}`);
-    pauseBtn.classList.remove("hide");
-    playBtn.classList.add("hide");
-});
-
-pauseBtn.addEventListener("click",(e)=>{
-    e.preventDefault();
-    //var music = new Audio("music1.mp3");
-    music.pause();
-    pauseBtn.classList.add("hide");
-    playBtn.classList.remove("hide");
-});
 
 
 alert("Hello")
